0aee1028a4af3c33ad7d152adbf3d4452ace5ad7
Ekaterina Popova <katerina.shavonsky@yandex.ru>
2020-08-30 20:48:49 +0300

    BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
 
-        int BonusMilesService = service.calculate(50_000);
-        System.out.println(BonusMilesService);
+        int bonusMilesService = service.calculate(50_000);
+        System.out.println(bonusMilesService);
 
-        int BonusMilesService1 = service.calculate(10_000);
-        System.out.println(BonusMilesService1);
+        int bonusMilesService1 = service.calculate(10_000);
+        System.out.println(bonusMilesService1);
 
-        int BonusMilesService2 = service.calculate(500_000);
-        System.out.println(BonusMilesService2);
+        int bonusMilesService2 = service.calculate(500_000);
+        System.out.println(bonusMilesService2);
 
-        int BonusMilesService3 = service.calculate(1_000_000);
-        System.out.println(BonusMilesService3);
+        int bonusMilesService3 = service.calculate(1_000_000);
+        System.out.println(bonusMilesService3);
     }
 
 }
